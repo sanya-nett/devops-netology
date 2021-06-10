fb610f43183d000f25d8471776c193b22a4fb42a
Alexander Scherba <scherba.alexander@gmail.com>
2021-06-10 16:03:56 +0300

    fixed

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -15,13 +15,13 @@ output "aws_region_name" {
 }
 
 output "ec2_private_ip" {
-  value = aws_instance.web[0].private_ip
+  value = aws_instance.web.private_ip
 }
 
 output "ec2_public_ip" {
-  value = aws_instance.web[0].public_ip
+  value = aws_instance.web.public_ip
 }
 
 output "ec2_subnet_id" {
-  value = aws_instance.web[0].subnet_id
+  value = aws_instance.web.subnet_id
 }
